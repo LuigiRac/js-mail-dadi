e05e25242cf9ba47c2300cefeeb17efc7a1e50cf
LuigiRac <lui.rac06@gmail.com>
2024-11-06 00:50:01 +0100

    generate array mail

--- script/script.js
+++ script/script.js
@@ -8,3 +8,16 @@ stampa un messaggio appropriato sull’esito del controllo.
 Non è necessario provvedere alla validazione delle email
 */
 
+// Obiettivo: Chiedere all'utente la sua mail 
+
+// Raccolta dati:
+
+
+const userMail = [
+    "adriano@mail.it",
+    "luca34@gmail.com",
+    "marco@libero.it",
+    "edoardo50@gmail.com",
+    "gabriele@tiscali.it",
+    "stefania@tim.com"
+  ]
