27518809bf0ad86f8015c99ab85bf51c9226990f
LuigiRac <lui.rac06@gmail.com>
2024-11-06 00:37:31 +0100

    start 1 exercise

---
+++ script/script.js
@@ -0,0 +1,10 @@
+"use strict"
+console.clear()
+
+/* 
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Non è necessario provvedere alla validazione delle email
+*/
+
