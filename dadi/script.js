1cf54453649c0fc925e7405d4de33eb64de9a6ad
LuigiRac <lui.rac06@gmail.com>
2024-11-06 02:11:39 +0100

    end "dadi" exercise

--- dadi/script.js
+++ dadi/script.js
@@ -14,5 +14,16 @@ function rollDice() {
   
   const randomNumerUser = rollDice();
   const randomNumerPc = rollDice();
-  console.log(rollDice());
+  console.log(randomNumerUser, randomNumerPc);
   
+  if (randomNumerUser > randomNumerPc) {
+    console.log("Utente è il vincitore");
+    
+  }
+  else if (randomNumerUser < randomNumerPc) {
+        console.log("Computer è il vincitore");
+ }
+ else {
+    console.log("PAREGGIO!!!");
+    
+ }
