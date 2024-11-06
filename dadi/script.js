793c75d7972a041dc7abbddfbb0ef96fc112f7e9
LuigiRac <lui.rac06@gmail.com>
2024-11-06 01:23:45 +0100

    user number prompt

--- dadi/script.js
+++ dadi/script.js
@@ -7,4 +7,5 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
-
+let userNumber = prompt("Utente: scegli il tuo numero fortunato da 1 a 6")
+  console.log ("Il numero inserito è: " + userNumber)
