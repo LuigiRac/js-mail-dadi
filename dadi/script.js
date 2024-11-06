7930df6523b8ba4acbb49066e8459f3f9e1fcadd
LuigiRac <lui.rac06@gmail.com>
2024-11-06 01:27:52 +0100

    fix const

--- dadi/script.js
+++ dadi/script.js
@@ -7,5 +7,18 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
-let userNumber = prompt("Utente: scegli il tuo numero fortunato da 1 a 6")
-  console.log ("Il numero inserito è: " + userNumber)
+
+const userNumber = [
+    "1","2","3","4","5","6"
+  ]
+
+let number = prompt("Utente: scegli il tuo numero fortunato da 1 a 6")
+  console.log ("Il numero inserito è: " + number);
+
+  if (userNumber.includes(number)){
+    console.log("Numero valido");
+ } 
+ 
+ 
+ 
+ else {}
